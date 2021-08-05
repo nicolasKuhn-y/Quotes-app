ad3fa42520e380f09d16e4a4144382e2dc13f458
Nicolas Kuhn <nicolaskuhn02@gmail.com>
2021-08-04 21:57:28 -0300

    contenedores de componentes añadidos

---
+++ src/containers/ButtonContainer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ButtonContainer = ({ children }) => (
+  <div className="quote-btns__wrapper">{children}</div>
+);
---
+++ src/containers/QuoteContainer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { useCounter } from "../hooks/useCounter";
+
+import { ButtonContainer } from "./ButtonContainer";
+
+import { Button } from "../components/Button";
+import { CharacterImage } from "../components/CharacterImage";
+import { Quote } from "../components/Quote";
+
+export const QuoteContainer = ({ data }) => {
+  const { counter: quoteNumber, increment, decrement, reset } = useCounter();
+
+  const { author, quote } = !!data && data[quoteNumber];
+  const totalQuotes = !!data && data.length - 1;
+
+  return (
+    <section className="quote__Container">
+      <CharacterImage query={author} />
+
+      <Quote author={author} quote={quote}>
+        <ButtonContainer
+          quoteNumber={quoteNumber}
+          totalQuotes={totalQuotes}
+          actions={[decrement, reset, increment]}
+        >
+          <Button onClickAction={decrement} disabled={quoteNumber === 0}>
+            &larr;
+          </Button>
+
+          <Button onClickAction={reset}>Reset</Button>
+
+          <Button
+            onClickAction={increment}
+            disabled={quoteNumber === totalQuotes}
+          >
+            &rarr;
+          </Button>
+        </ButtonContainer>
+      </Quote>
+    </section>
+  );
+};
